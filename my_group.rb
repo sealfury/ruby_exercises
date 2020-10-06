5eb08121bde237f3221f0b30ea3ef0e1c738e356
William <willspecker@gmail.com>
2020-10-06 21:51:48 +0200

    Adding an array with three hashes containing three objects

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+person_1 = {name: "William", gender: "male", age: 33}
+person_2 = {name: "Mathilda", gender: "female", age: 33}
+person_3 = {name: "Stina", gender: "kott", age: 6}
+my_group = [person_1, person_2, person_3]
