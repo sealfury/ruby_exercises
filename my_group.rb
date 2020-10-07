ef2347dc7286e225bc54b4db20a1673dc22a897b
William <willspecker@gmail.com>
2020-10-07 17:02:15 +0200

    Adding changes made for the ruby special exercise to generate sentences using specific keys and values in the array

--- my_group.rb
+++ my_group.rb
@@ -1,4 +1,8 @@
-person_1 = {name: "William", gender: "male", age: 33}
-person_2 = {name: "Mathilda", gender: "female", age: 33}
+person_1 = {name: "William", gender: "man", age: 33}
+person_2 = {name: "Mathilda", gender: "woman", age: 33}
 person_3 = {name: "Stina", gender: "kott", age: 6}
 my_group = [person_1, person_2, person_3]
+
+my_group.each do |person|
+    puts "#{person[:name]} is a #{person[:age]} year old #{person[:gender]}"
+end
