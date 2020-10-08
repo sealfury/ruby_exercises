ee047bc1bb30138835d1e7d548d8123b75998795
William <willspecker@gmail.com>
2020-10-08 17:01:28 +0200

    Writing a program called age.rb that asks a user how old they are and then tells them how old they will be in 10, 20, 30, and 40 years

---
+++ age.rb
@@ -0,0 +1,9 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+if age > 0
+    puts "In ten years you will be #{age+10} years old"
+    puts "In twenty years you will be #{age+20} years old"
+    puts "In thirty years you will be #{age+30} years old"
+    puts "In forty years you will be #{age+40} years old"
+end
+
